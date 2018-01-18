3f43147b7f0cd4508b4e14a781441ef9ecbfa194
Adriaan Marain <adriaanmarain300@gmail.com>
2018-01-18 12:32:32 +0100

    google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -122,5 +122,14 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
     @yield('page-scripts')
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112690615-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-112690615-1');
+    </script>
 </body>
 </html>
